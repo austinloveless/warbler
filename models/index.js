acd9c57db0d611fc7c3a6858e3f29229e90fab3d
austinloveless <austinloveless5171@gmail.com>
2018-08-22 14:59:18 -0600

    mongodb

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@ mongoose.set("debug", true);
 mongoose.Promise = Promise;
 
 mongoose.connect(
-    "mongodb://localhost/warbler",
-    {
-        useNewUrlParser: true,
-        keepAlive: true
-    }
+  "mongodb://warbler:warbler5171@ds163020.mlab.com:63020/warbler",
+  {
+    useNewUrlParser: true,
+    keepAlive: true
+  }
 );
 
 module.exports.User = require("./user");
